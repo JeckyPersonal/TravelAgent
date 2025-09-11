c321a52d2d4809d6e9a20d8b864ad007e1686458
Jecky <kansara.jecky@gmail.com>
2025-09-11 14:06:12 +0530

    Applied nullable for non-required fields.

--- Backend/Invoice/DTO/CompanyDto.cs
+++ Backend/Invoice/DTO/CompanyDto.cs
@@ -6,21 +6,21 @@ namespace Invoice.DTO
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The 'Name' field is required. Please provide a name and try again.")]
         public string Name { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string? Address3 { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The 'GSTNo' field is required. Please provide a name and try again.")]
         public string GSTNo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The 'PANNo' field is required. Please provide a name and try again.")]
         public string PANNo { get; set; }
-        public string PhoneNumber { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
-        public string Zip { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? Zip { get; set; }
     }
 }
