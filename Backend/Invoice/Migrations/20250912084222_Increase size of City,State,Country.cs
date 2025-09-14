28d22f647c397aa46999ecbf8cbdd4dd975e13f4
Jecky <kansara.jecky@gmail.com>
2025-09-14 17:17:26 +0530

    Add migration file

---
+++ Backend/Invoice/Migrations/20250912084222_Increase size of City,State,Country.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoice.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasesizeofCityStateCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "state",
+                table: "company",
+                type: "varchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "city",
+                table: "company",
+                type: "varchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "state",
+                table: "company",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "city",
+                table: "company",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+        }
+    }
+}
