c54116de881f3015fb3565890cd3c5231118b7ed
Jecky <kansara.jecky@gmail.com>
2025-09-15 23:41:58 +0530

    add bank and customer controller

---
+++ Backend/Invoice/Controllers/BankController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Invoice.DTO;
+using Invoice.Model;
+using Invoice.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Invoice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankController : ControllerBase
+    {
+
+        private readonly IService<Bank> _bankService;
+        private readonly IMapper _autoMapper;
+
+        public BankController(IService<Bank> bankService, IMapper autoMapper)
+        {
+            _bankService = bankService;
+            _autoMapper = autoMapper;
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<BankDto>>> GetAll()
+        {
+            List<Bank> banks = await this._bankService.GetAll();
+
+            if (banks.Count == 0) return NoContent();
+
+            List<BankDto> bankResponse = banks.Select(x=> this._autoMapper.Map<BankDto>(x)).ToList();
+
+            return Ok(bankResponse);
+        }
+
+        [HttpGet]
+        [Route("get/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<CompanyDto>> Get(int id)
+        {
+            if (id <= 0)
+            {
+                ModelStateDictionary dic = new ModelStateDictionary();
+                dic.TryAddModelError("Id", "Id should be grater then zero. Please re-try with non zero id.");
+                return BadRequest(new ValidationProblemDetails(dic));
+            }
+
+            Bank companyById = await this._bankService.Get(id);
+
+            if (companyById == null)
+                return NoContent();
+
+            return Ok(companyById);
+        }
+    }
+}
---
+++ Backend/Invoice/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Invoice.DTO;
+using Invoice.Model;
+using Invoice.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invoice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly IService<Customer> _companyService;
+        private readonly IMapper _autoMapper;
+
+        [HttpPost]
+        [Route("add")]
+        public ActionResult<CustomerDto> Add([FromBody] CustomerDto company)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        [Route("update/{id:int}")]
+        public ActionResult<CustomerDto> Update(int id, [FromBody] CustomerDto company)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("get/{id:int}")]
+        public ActionResult<CustomerDto> Get(int id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("get/{name:alpha}")]
+        public ActionResult<CustomerDto> Get(string name)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        public ActionResult<IEnumerable<CustomerDto>> GetAll()
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("delete/{id:int}")]
+        public ActionResult<CustomerDto> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
