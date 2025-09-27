dc48da922258869622448b889bd318387760acce
Jecky <kansara.jecky@gmail.com>
2025-09-27 14:21:00 +0530

    Add ItemMasterDto

---
+++ FrontEnd/Invoice.UI/Invoice.UI/DTO/ItemMasterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice.UI.DTO
+{
+    public class ItemMasterDto
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public double Rate { get; set; }
+        public bool AppliedGST { get; set; }
+    }
+}
