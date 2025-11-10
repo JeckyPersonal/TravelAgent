bdaebddd8716e046b6a5ff0bd1e3200426929df3
Kinjal Patel <kinjal.patel@automationanywhere.com>
2025-11-10 21:32:06 +0530

    Reading setting from appsettings.json

--- FrontEnd/Invoice.UI/Invoice.UI/Program.cs
+++ FrontEnd/Invoice.UI/Invoice.UI/Program.cs
@@ -3,6 +3,7 @@ using Invoice.UI.CompanySelector;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Invoice.UI
@@ -19,45 +20,14 @@ namespace Invoice.UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //// 1️⃣ Build configuration
-            //var builder = new ConfigurationBuilder() 
-            //    .SetBasePath(AppContext.BaseDirectory) 
-            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) .Build(); 
-            //// 2️⃣ Create service collection (DI container)
-            //var services = new ServiceCollection(); 
-            //// 3️⃣ Bind config section to strongly typed class
-            //services.Configure<CurrentConfig>(builder.GetSection("CurrentConfig")); 
-            //// 4️⃣ Register your main form
-            //services.AddTransient<Form>();
-
-            //// 5️⃣ Build the provider and run the app
-            //using (ServiceProvider serviceProvider = services.BuildServiceProvider()) 
-            //{
-
-            //    CompanySelectorPresenter presenter = new CompanySelectorPresenter(CompanyRestClient.Instance, FinancialYear.FinancialYearRestClient.Instance);
-            //    presenter.SetView(new CompanySelector.CompanySelector());
-            //    presenter.ShowUI();
-
-            //    if (presenter.GetView().DialogResult == DialogResult.OK)
-            //    {
-            //        var form = serviceProvider.GetRequiredService<Form>();
-            //        ApplicationConfiguration.Initialize();
-            //        Application.Run(presenter.GetNextView());
-
-            //        Application.Run(presenter.GetNextView());
-            //    }
-            //}
-
-            ///*consumer who want to use setings
-
-            //private readonly CurrentConfig _config;
-
-            //public Form1(IOptions<CurrentConfig> options)
-            //{
-            //    InitializeComponent();
-            //    _config = options.Value;
-            //}
-            //*/
+            // Build configuration
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // path to bin/debug
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            // Read values
+            string apiUrl = config["CurrentConfig:ApiUrl"];
+            Settings.BaseUrl = apiUrl;
 
             CompanySelectorPresenter presenter = new CompanySelectorPresenter(CompanyRestClient.Instance, FinancialYear.FinancialYearRestClient.Instance);
             presenter.SetView(new CompanySelector.CompanySelector());
