fec4e5e2fd357e7f872f5780c097131cb27ffab3
Jecky <kansara.jecky@gmail.com>
2025-11-25 21:05:23 +0530

    add invoice payment configuration

---
+++ Backend/Invoice/Model/Config/InvoicePaymentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invoice.Model.Config
+{
+    public class InvoicePaymentConfiguration : IEntityTypeConfiguration<InvoicePayment>
+    {
+        public void Configure(EntityTypeBuilder<InvoicePayment> builder)
+        {
+            builder.ToTable("invoice_payment");
+
+            builder.HasKey(x => new { x.InvoiceId, x.PaymentId });
+
+            builder.HasOne(x=> x.Invoice).WithMany(x=> x.InvoicePayments).HasForeignKey(x=> x.InvoiceId).HasConstraintName("FK_INVOICE_INVOICE_PAYMENT");
+
+            builder.HasOne(x => x.Payment).WithMany(x => x.InvoicePayments).HasForeignKey(x => x.PaymentId).HasConstraintName("FK_PAYMENT_INVOICE_PAYMENT");
+        }
+    }
+}
